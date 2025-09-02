734d9bc176d383cd5b62991c66193c2952c3ae66
felipe-figueiro <felipe.figueiro@aluno.senai.br>
2025-09-02 08:35:59 -0300

    lancamento

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 22;
+let num4 = 33;
+
+let media = (num1 + num2 + num3 + num4) / 4;
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log("Calculadora no Terminal");
+
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
+console.log("valores para média:", num1,",", num2,",", num3,",", num4);
+console.log("Média:", media);
