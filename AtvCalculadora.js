f6dfac27d26bdd6e1182ba6f6d4a568c5cca20a6
felipe-figueiro <felipe.figueiro@aluno.senai.br>
2025-09-02 10:15:29 -0300

    Atividade_Calculadora

--- AtvCalculadora.js
+++ AtvCalculadora.js
@@ -4,11 +4,11 @@ let num2 = 2;
 let num3 = 22;
 let num4 = 33;
 
-let media = (num1 + num2 + num3 + num4) / 4;
 let soma = num1 + num2;
 let subtracao = num1 - num2;
 let multiplicacao = num1 * num2;
 let divisao = num1 / num2;
+let media = (num1 + num2 + num3 + num4) / 4;
 
 console.log("Calculadora no Terminal");
 
